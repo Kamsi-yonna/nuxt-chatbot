f775da7025180e4cb7cae344dcbe1df4de350cf2
Kamsi-yonna <etiobikamsi@gmail.com>
2025-01-15 02:21:58 +0100

    chore - set app head and meta tags (not needed)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,14 +5,29 @@ export default defineNuxtConfig({
   pages: true,
 
   ssr: true,
-  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", ["@nuxt/ui", {}]],
 
+
+   app: {
+     head: {
+       title: "AI CHATBOT",
+       meta: [
+         { name: 'description', content: "A simple AI-powered query assistant built with Nuxt 3, TypeScript, and Tailwind CSS." },
+         { property: 'og:title', content: "NUXT AI CHATBOT" },
+         { property: 'og:description', content: "Explore a minimal yet functional demonstration of integrating together.xyz's API into a Nuxt application." },
+         { name: 'twitter:title', content: "NUXT AI CHATBOT" },
+         { name: 'twitter:description', content: "A simple AI-powered query assistant built with Nuxt 3, TypeScript, and Tailwind CSS." },
+         { name: 'twitter:image', content: "/favicon.ico" }, 
+        ],
+      }},
+      
   icon: {
     serverBundle: {
       collections: ["uil", "mdi"],
     },
   },
-
+  
+  modules: ["@nuxtjs/tailwindcss", "nuxt-icon", ["@nuxt/ui", {}]],
+  
   runtimeConfig: {
     TOGETHER_API_KEY: process.env.NUXT_TOGETHER_API_KEY
   },
